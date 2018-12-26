2a3c34b27e42af571e3594d9e076cb5f825629df
Igor Mišić <igy1000mb@gmail.com>
2018-12-26 18:05:42 +0100

    Support for temperature and time stamp

--- web/menu.php
+++ web/menu.php
@@ -1,5 +1,32 @@
 <?php
 
+$temp;
+$timeStamp;
+
+function get_temp()
+{
+
+    global $temp;
+    global $timeStamp;
+    global $conn;
+
+    $result = $conn->query("SELECT temp, ts FROM temp WHERE id = 1");
+
+    if ($result->num_rows > 0) 
+    {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            $temp = $row["temp"];  
+            $timeStamp = $row["ts"];
+        }
+    } 
+    else 
+    {
+        echo "No temp in table<br>";
+    }
+}
+
+
 if (!isset($_GET["showGuests"]) )
 {
     $checkBoxMenuHtml = "
@@ -15,14 +42,18 @@ else
     </form>";
 }
 
+get_temp();
+
 echo 
 "<table  class=menu >
     <tr>
         <td width=15%><b><a href=\"index.php\">Home</a></b></td>
         <td width=15%><b><a href=\"admin_panel.php\">Admin</a></b></td>
-        <td width=15%><b><a href=\"smart_lock_log.php\">Smart lock</a></b></td>   
         $checkBoxMenuHtml
-        <td width=60%></td>";
+        <td width=15%><b><a href=\"smart_lock_log.php\">Smart lock</a></b></td>
+        <td width=5%><b>".$temp." &#8451;</b></td>
+        <td width=30%></td>
+        <td width=10%>".$timeStamp."</td>";
         
 //if (isset($_COOKIE["TOKEN"])) 
 //echo "<th><a href=\"index.php?logout=true\">Logout</th>";
---
+++ web/temp_api.php
@@ -0,0 +1,51 @@
+<?php
+// API for collecting MAC addresses
+include 'database_config.php';
+
+//key must be same as in bash script
+$result = $conn->query("SELECT psk FROM preshared_key WHERE name='api_psk'");
+
+if ($result->num_rows > 0) 
+{
+    // output data of each row
+    while($row = $result->fetch_assoc()) 
+    {
+        $key = $row["psk"];
+    }
+} 
+else 
+{
+    echo "0 results";
+}
+
+$validator = $_GET["validator"];
+$date = $_GET["date"];
+$temp = "";
+
+$totalSpentTime = '';
+
+$calculated_validator = hash_hmac ('sha1', $date, $key);
+
+if(strcmp($calculated_validator,$validator) === 0)
+{
+    if ($conn->connect_errno) 
+    {
+      exit('Can\'t connect : '. $conn->connect_error);
+    }
+
+    $temp = $_GET["temp"];
+    if ($conn->query("UPDATE temp SET temp='$temp', ts=current_timestamp WHERE id =1") === TRUE) 
+    {
+        echo "New record created successfully<br>";
+        
+    } 
+    else 
+    {
+        echo "UPDATE temp SET temp='$temp' WHERE id = 1 <br>" . $conn->error."<br>";
+    }
+
+}
+else echo "Validator is wrong";
+
+$conn->close();
+?>
